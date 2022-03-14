803b99a283305e313e40e8def932a779c2a6aa56
Kiran Bhosale <91077158+kiran04121997@users.noreply.github.com>
2022-03-14 08:33:22 +0530

    updated

---
+++ OopsConceptsProject/OopsConcepts/Encapsulation/Person.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+            Console.ReadLine();
+           
+        }
+    }
+}
