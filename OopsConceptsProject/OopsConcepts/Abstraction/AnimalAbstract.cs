884cbf7c2c20c14034e13aa7a37802cd03f146c3
Kiran Bhosale <91077158+kiran04121997@users.noreply.github.com>
2022-03-14 08:24:03 +0530

    Updated

--- OopsConceptsProject/OopsConcepts/Abstraction/AnimalAbstract.cs
+++ OopsConceptsProject/OopsConcepts/Abstraction/AnimalAbstract.cs
@@ -1,17 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OopsConcepts.Abstraction
+namespace Abstraction
 {
-    abstract class AnimalAbstract
+    // Abstract class
+    abstract class Animal
     {
-        public abstract void AnimalSound1();
+        // Abstract method (does not have a body)
+        public abstract void animalSound();
+        // Regular method
         public void sleep()
         {
-            Console.WriteLine("Non abstract method");
+            Console.WriteLine("Zzz");
         }
+    }
+
+    // Derived class (inherit from Animal)
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); // Create a Pig object
+            myPig.animalSound();  // Call the abstract method
+            myPig.sleep();  // Call the regular method
+            Console.ReadLine();
+        }
+
     }
 }
